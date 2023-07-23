49e82388e8f00d84c271c4709e2221a048110ff6
MD MONJUR MORSHED <mmt3719@gmail.com>
2023-07-23 12:12:19 +0600

    step 2

--- lib/models/bidproduct_model.dart
+++ lib/models/bidproduct_model.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class BidProduct {
+  String productId;
   String productName;
   String productDescription;
   double bidMinPrice;
@@ -9,5 +12,16 @@ class BidProduct {
       required this.productDescription,
       required this.bidMinPrice,
       required this.bidDateTime,
-      required this.productImage});
+      required this.productImage,
+      required this.productId});
+
+  factory BidProduct.fromDocument(DocumentSnapshot<Map<String, dynamic>> doc) {
+    return BidProduct(
+        productName: doc['productName'],
+        productDescription: doc['productDescription'],
+        bidMinPrice: double.parse(doc['productMinBidPrice']),
+        bidDateTime: doc['bidExpiryDate'],
+        productImage: doc['productImage'],
+        productId: doc.id);
+  }
 }
